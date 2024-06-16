a0b654143c0032c10aa5a183f50cc5a31e2b12c4
Martine Alliet <allietmartine@gmail.com>
2024-06-16 22:02:06 +0100

    Chapter3First

---
+++ Chapter3/BasicDataTypes/Program.cs
@@ -0,0 +1,193 @@
+﻿using System.Numerics;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("**** Fun with Basic Data Types");
+LocalVarDeclarations();
+DefautDeclarations();
+NewingDataTypes();
+ObjectFunctionality();
+DataTypeFunctionality();
+CharFunctionality();
+ParsingFromString();
+ParseFromStringWithTryParse();
+UseDatesAndTimes();
+UseBigInteger();
+DigitSeparators();
+BinaryLiterals();
+
+void BinaryLiterals()
+{
+    Console.WriteLine("=> Use Binary Literals:");
+    Console.WriteLine("Sixteen: {0}", 0b0001_0000);
+    Console.WriteLine("Thirty Two: {0}", 0b0010_0000);
+    Console.WriteLine("Sixty Four: {0}", 0b0100_0000);
+}
+
+void DigitSeparators()
+{
+    Console.WriteLine("=> Use Digit Separators:");
+    Console.WriteLine("Integer:");
+    Console.WriteLine(123_456);
+    Console.WriteLine("Long:");
+    Console.WriteLine(123_456_789L);
+    Console.WriteLine("Float:");
+    Console.WriteLine(123_456.1234F);
+    Console.WriteLine("Double:");
+    Console.WriteLine(123_456.12);
+    Console.WriteLine("Decimal:");
+    Console.WriteLine(123_456.12M);
+    Console.WriteLine("Hex:");
+    Console.WriteLine(0x2A_2B_2C);
+    Console.WriteLine();
+}
+
+void UseBigInteger()
+{
+    Console.WriteLine("=> Use BigInteger:");
+    BigInteger biggy = BigInteger.Parse("999999999999999999999999999999999999999999999999999");
+    Console.WriteLine("Value of biggy is {0}", biggy);
+    Console.WriteLine("Is biggy an even value?: {0}", biggy.IsEven);
+    Console.WriteLine("Is biggy a power of two?: {0}", biggy.IsPowerOfTwo);
+    BigInteger reallyBig = BigInteger.Multiply(biggy, BigInteger.Parse("88888888888888888888888888888888888888"));
+    Console.WriteLine("Value of reallyBig is {0}", reallyBig);
+}
+
+void UseDatesAndTimes()
+{
+    Console.WriteLine("=> Dates and Times:");
+
+    //This constructor takes (year, month, day)
+    DateTime dt = new DateTime(2024, 03, 14);
+    //What day of the month is this?
+    Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
+    //Month is now December
+    dt = dt.AddMonths(2);
+    Console.WriteLine("Daylight savings: {0}", dt.IsDaylightSavingTime());
+
+    //This constructor takes (hours, minutes, seconds)
+    TimeSpan ts = new TimeSpan(4, 30, 0);
+    Console.WriteLine(ts);
+    //Subtract 15 minutes from the current TimeSpan and
+    //print the result
+    Console.WriteLine(ts.Subtract(new TimeSpan(0, 15, 0)));
+
+
+
+}
+
+void ParseFromStringWithTryParse()
+{
+    Console.WriteLine("=> Data type parsing with TryParse:");
+    if (bool.TryParse("True", out bool b))
+    {
+        Console.WriteLine("Value of b: {0}", b);
+    }
+    else
+    {
+        Console.WriteLine("Value could not be parsed");
+    }
+    string value = "Hello";
+    if (double.TryParse(value, out double d))
+    {
+        Console.WriteLine("Value of d: {0}", d);
+    }
+    else
+    {
+        Console.WriteLine("Value could not be parsed");
+    }
+    Console.WriteLine();
+}
+
+void ParsingFromString()
+{
+    Console.WriteLine("=> Data type parsing:");
+    bool b = bool.Parse("True");
+    Console.WriteLine("Value of b: {0}", b);
+    double d = double.Parse("99.884");
+    Console.WriteLine("Value of d: {0}", d);
+    int i = int.Parse("8");
+    Console.WriteLine("Value of i: {0}", i);
+    char c = Char.Parse("w");
+    Console.WriteLine("Value of c: {0}", c);
+    Console.WriteLine();
+}
+
+void CharFunctionality()
+{
+    Console.WriteLine("=> char type Functionality:");
+    char myChar = 'a';
+    Console.WriteLine("char.IsDigit('a'): {0}", char.IsDigit(myChar));
+    Console.WriteLine("char.IsLetter('a'): {0}", char.IsLetter(myChar));
+    Console.WriteLine("char.IsWhiteSpace('Hello There', 5): {0}", char.IsWhiteSpace("Hello There", 5));
+    Console.WriteLine("char.IsWhiteSpace('Hello There', 6): {0}", char.IsWhiteSpace("Hello There", 6));
+    Console.WriteLine("char.IsPunctuation('?'): {0}", char.IsPunctuation('?'));
+    Console.WriteLine();
+}
+
+void DataTypeFunctionality()
+{
+    Console.WriteLine("=> Data Type Functionality:");
+
+    Console.WriteLine("Max of int: {0}", int.MaxValue);
+    Console.WriteLine("Min of int: {0}", int.MinValue);
+    Console.WriteLine("Max of double: {0}", double.MaxValue);
+    Console.WriteLine("Min of double: {0}", double.MinValue);
+    Console.WriteLine("double.Epsilon: {0}", double.Epsilon);
+    Console.WriteLine("double.PositiveInfinity: {0}", double.PositiveInfinity);
+    Console.WriteLine("double.NegativeInfinity: {0}", double.NegativeInfinity);
+    Console.WriteLine("bool.FalseString: {0}", bool.FalseString);
+    Console.WriteLine("bool.TrueString: {0}", bool.TrueString);
+    Console.WriteLine();
+}
+
+void ObjectFunctionality()
+{
+    Console.WriteLine("=> System.Object Functionality:");
+    //A C# int is really a shorthand for System.Int32,
+    //which inherits the following members from System.Object.
+    Console.WriteLine("12.GetHashCode() = {0}", 12.GetHashCode());
+    Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
+    Console.WriteLine("12.ToString() = {0}", 12.ToString());
+    Console.WriteLine("12.GetType() = {0}", 12.GetType());
+    Console.WriteLine();
+
+}
+
+static void LocalVarDeclarations()
+{
+    Console.WriteLine("=> Data Declarations:");
+    // Local variables are declared using the following syntax:
+    // dataType varName = initialValue;
+    int myInt = 0;
+    string myString;
+    myString = "This is my character data";
+    //declare three bools on a single line
+    bool b1 = true, b2 = false, b3 = b1;
+    //use System.Boolean data type to declare a bool
+    System.Boolean b4 = false;
+    Console.WriteLine("Your data: {0}, {1}, {2}, {3}, {4}, {5}", myInt, myString, b1, b2, b3, b4);
+    Console.WriteLine();
+}
+
+static void DefautDeclarations()
+{
+    Console.WriteLine("=> Default Declarations:");
+    int myInt = default;
+    bool myBool = default;
+    char myChar = default;
+    Console.WriteLine("Default int: {0}", myInt);
+    Console.WriteLine("Default bool: {0}", myBool);
+    Console.WriteLine("Default char: {0}", myChar);
+    Console.WriteLine();
+}
+
+static void NewingDataTypes()
+{
+    Console.WriteLine("=> Using new to create variables:");
+    bool b = new bool(); //set to false
+    int i = new int(); //set to 0
+    double d = new double(); //set to 0
+    DateTime dt = new DateTime(); //set to 1/1/0001
+    Console.WriteLine("{0}, {1}, {2}, {3}", b, i, d, dt);
+    Console.WriteLine();
+}
---
+++ Chapter3/TypeConversions/Program.cs
@@ -0,0 +1,168 @@
+﻿
+
+Console.WriteLine("***** Fun with type conversions *****\n");
+// Add two shorts and print the result.
+short numb1 = 9, numb2 = 10;
+Console.WriteLine("{0} + {1} = {2}", numb1, numb2, Add(numb1, numb2));
+Console.WriteLine();
+
+// compiler error below!
+//short numb3 = 3000000000000000, numb4 = 30000;
+//Console.WriteLine("{0} + {1} = {2}", numb3, numb4, Add(numb3, numb4));
+//Console.WriteLine();
+
+//Another compiler error!
+//static void NarrowingAttempt()
+//{
+//    byte myByte = 0;
+//    int myInt = 200;
+//    myByte = myInt;
+//    Console.WriteLine("Value of myByte: {0}", myByte);
+//}
+
+short numb3 = 30000, numb4 = 30000;
+Console.WriteLine("{0} + {1} = {2}", numb3, numb4, (short)Add(numb3, numb4));
+Console.WriteLine();
+NarrowingAttempt();
+ProcessBytes();
+LinqQueryOverInts();
+IfElsePatternMatchingUpdatedInCSharp();
+ExecuteIfElseUsingConditionalOperator();
+ConditionalRefExample();
+ExecutePatternMatchingSwitchWithWhen();
+
+void ExecutePatternMatchingSwitchWithWhen()
+{
+    Console.WriteLine("***** Fun with Pattern Matching *****\n");
+    Console.WriteLine("1 [C#], 2 [VB]");
+    Console.Write("Please pick your language preference: ");
+
+    string langChoice = Console.ReadLine();
+
+    object choice = langChoice == "1" ? (object)"C#" : "VB";
+
+    switch (choice)
+    {
+        case int i when i == 2:
+        case string s when s.Equals("VB", StringComparison.OrdinalIgnoreCase):
+            Console.WriteLine("VB:OOP, multithreading, and more!");
+            break;
+        case int i when i == 1:
+        case string s when s.Equals("C#", StringComparison.OrdinalIgnoreCase):
+            Console.WriteLine("Good choice, C# is a fine language");
+            break;
+        default:
+            Console.WriteLine("Well...good luck with that!");
+            break;
+    }
+    Console.WriteLine();
+}
+
+void ConditionalRefExample()
+{
+    var smallArray = new int[] { 1, 2, 3, 4, 5 };
+    var largeArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int index = 7;
+    ref int refValue = ref ((index < 5) ? ref smallArray[index] : ref largeArray[index]);
+    refValue = 0;
+    index = 2;
+    ((index < 5) ? ref smallArray[index] : ref largeArray[index]) = 100;
+    Console.WriteLine("smallArray[2] = " + smallArray[2]);
+    Console.WriteLine("largeArray[7] = " + largeArray[7]);
+
+    Console.WriteLine();
+    Console.WriteLine(string.Join(", ", smallArray));
+    Console.WriteLine(string.Join(", ", largeArray));
+}
+
+void ExecuteIfElseUsingConditionalOperator()
+{
+    string stringData = "My textual data";
+    Console.WriteLine(stringData.Length > 0 ? "string is greater than 0" : "string is not greater than 0");
+    Console.WriteLine();
+
+    //stringData.Length > 0 ? Console.WriteLine("string is greater than 0") : Console.WriteLine("string is not greater than 0");
+
+    Console.WriteLine();
+
+}
+
+void IfElsePatternMatchingUpdatedInCSharp()
+{
+    Console.WriteLine("***** Fun with Pattern Matching *****\n");
+    object testItem1 = 123;
+    Type t = typeof(string);
+    char c = 'f';
+
+    //Type patterns
+    if (t is Type)
+    {
+        Console.WriteLine($"{t} is a Type");
+    }
+
+    //Relational, conjuctive, and disjunctive patterns
+    if (c is >= 'a' and <= 'z' or >= 'A' and <= 'Z')
+    {
+        Console.WriteLine($"{c} is a character");
+    }
+
+    //Parenthesized patterns
+    if (c is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z'))
+    {
+        Console.WriteLine($"{c} is a character or separator");
+    }
+
+    //Negative patterns
+    if (testItem1 is not string)
+    {
+        Console.WriteLine($"{testItem1} is not a string");
+    }
+
+    if (testItem1 is not null)
+    {
+        Console.WriteLine($"{testItem1} is not null");
+    }
+}
+
+void LinqQueryOverInts()
+{
+    int[] ints = { 10, 45, 15, 39, 21, 26 };
+    var subset = from i in ints where i < 20 select i;
+    Console.Write("Values in subset: ");
+    foreach (var i in subset)
+    {
+        Console.Write("{0} ", i);
+    }
+    Console.WriteLine();
+    Console.WriteLine("subset is of type: {0}", subset.GetType().Name);
+    Console.WriteLine("subset is defined in: {0}", subset.GetType().Namespace);
+    Console.WriteLine();
+}
+
+void ProcessBytes()
+{
+    byte b1 = 100;
+    byte b2 = 250;
+    try
+    {
+        byte sum = checked((byte)Add(b1, b2));
+        Console.WriteLine("sum = {0}", sum);
+    }
+    catch (OverflowException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+static void NarrowingAttempt()
+{
+    byte myByte = 0;
+    int myInt = 200;
+    myByte = (byte)myInt;
+    Console.WriteLine("Value of myByte: {0}", myByte);
+}
+
+static int Add(int x, int y)
+{
+    return x + y;
+}
