b029f2a373664d07157480ab48d914619e516ccc
Martine Alliet <allietmartine@gmail.com>
2024-06-16 22:01:12 +0100

    Chapter2First

---
+++ Chapter_02/VisualStudio/SimpleCSharpConsoleApp/SimpleCSharpConsoleApp/Make.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCSharpConsoleApp
+{
+    public class Make
+    {
+        public string Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
