1ce7b45b2c79e9754b87797748d2d6f1b376f740
Martine Alliet <allietmartine@gmail.com>
2024-06-16 22:07:01 +0100

    Chapter10First

---
+++ Chapter10/FunWithObservableCollections/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace FunWithObservableCollections
+{
+    internal class Person
+
+    {
+        public Person()
+        {
+
+        }
+        public Person(string fName, string lName, int a)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = a;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, Age: {Age}";
+        }
+    }
+}
+
---
+++ Chapter10/FunWithObservableCollections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using FunWithObservableCollections;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** Fun with Observable Collections ****");
+ObservableCollection<Person> people = new ObservableCollection<Person>()
+{
+    new Person{FirstName = "Peter", LastName = "Murphy", Age = 52},
+    new Person{FirstName = "Kevin", LastName = "Key", Age = 48}
+};
+
+people.CollectionChanged += people_CollectionChanged;
+
+people.Add(new Person { FirstName = "Sally", LastName = "Smith", Age = 30});
+people.RemoveAt(0);
+
+
+void people_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+{
+    Console.WriteLine($"Action for this event: {e.Action}");
+    if (e.Action == NotifyCollectionChangedAction.Remove)
+    {
+        Console.WriteLine("Here are the OLD items:");
+        foreach (Person p in e.OldItems)
+        {
+            Console.WriteLine(p.ToString());
+        }
+        Console.WriteLine();
+    }
+    if (e.Action == NotifyCollectionChangedAction.Add)
+    {
+        Console.WriteLine("Here are the NEW items:");
+        foreach (Person p in e.NewItems)
+        {
+            Console.WriteLine(p.ToString());
+        }
+        Console.WriteLine();
+    }
+}
